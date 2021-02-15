4098a702e534650977474aa0669b847e88b64597
msmalinosterberg <msmalinosterberg@gmail.com>
2021-02-15 17:09:30 +0100

    new iconpic. Added  row with 1 column 2 containers

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,47 @@ void main() => runApp(FoodApp());
 class FoodApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Veckohandling',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Vad ska jag köpa?',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+        // Nästa rad
+      ),
+    );
     return MaterialApp(
       title: 'Inköpslista',
       home: Scaffold(
           appBar: AppBar(
               title: Text('Inköpslista'), backgroundColor: Colors.grey[800]),
-          body: Center(
-            child:
-                Text('Att köpa', style: TextStyle(fontWeight: FontWeight.bold)),
+          body: Column(
+            children: [
+              titleSection,
+            ],
           ),
           backgroundColor: Colors.grey[300]),
     );
